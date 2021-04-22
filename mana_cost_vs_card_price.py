d78e820ccc62af7b957dcf7f97edec4f2e582e66
Philip Nelson <philipnelson5@gmail.com>
2021-04-21 23:36:38 -0600

    card cost vs mana cost

---
+++ mana_cost_vs_card_price.py
@@ -0,0 +1,33 @@
+#%%
+from matplotlib import pyplot as plt
+import pickle
+from scipy import stats
+from cards import *
+
+pd.set_option('display.max_columns', None)
+cards = make_all_cards_df()
+
+#%%
+df = cards[
+    (cards['isReprint'].isna()) &
+    (cards['isPromo'].isna())
+  ].drop_duplicates(subset=['name']).dropna(subset=['prices'])
+x = []
+y = []
+for _, card in df.iterrows():
+    try:
+        y.append(list(card.prices['mtgo'].values())[0])
+        x.append(card.convertedManaCost)
+    except: pass
+
+
+#%%
+r, p = stats.pearsonr(x,y)
+rs = round(r, 2)
+ps = "{:.2e}".format(p)
+plt.scatter(x,y)
+plt.xlabel('Total Mana Cost (mana)')
+plt.ylabel('Card Price ($)')
+plt.title(f'Card Price vs Total Mana Cost\nr={rs}, p={ps}')
+plt.tight_layout()
+plt.savefig('images/card-cost-vs-mana-cost.pdf')
